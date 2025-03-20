2600e190e997ff8ff6ca1b6b97f56d0a66caeebd
Douglas Bastos <dautodouglasdauto@gmail.com>
2025-03-20 15:00:10 -0300

    Finalizado a função do botão e assim finalizado o projeto

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 (function () {
     const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green'];
     const btn = document.getElementById("btn");
+    const yourColor = document.getElementsByClassName('color')[0];
+    let oldValue = null;
+    
+    btn.addEventListener('click', () => {
+        let random = null;
+        let value = null;
+
+        generateValue();
+
+        if(oldValue !== null) {
+            while (oldValue === value) {
+                generateValue();
+            }
+        }
+
+        function generateValue() {
+            random = Math.random() * 4;
+            value = colors[random <= .5 ? Math.floor(random) : Math.ceil(random)];
+        }
+
+        document.body.style.backgroundColor = value;
+        yourColor.innerHTML = value;
+        oldValue = value;
+    });
 })();
